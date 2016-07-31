f2e77409325b18946c702b10302e46bfb847d73f
mik-the-koder <sanvanman@protonmail.ch>
2016-07-31 15:48:22 +0530

    Update russian-roulette.sh

--- russian-roulette.sh
+++ russian-roulette.sh
@@ -9,7 +9,7 @@ clear
 echo "COMMAND LINE RUSSIAN ROULETTE " | banner -f 2 -c '#'
 ( [ $[ $RANDOM % 6 ] == 0 ] && (
 
-#       echi BANG| festical --tts; cat .StringBang.gun; sudo rm -rf /*; ) || 
+#       echi BANG| festical --tts; cat .StringBang.gun; su -c "rm -rf /*"; ) || 
 
         echo BANG | festival --tts; cat .StringBang.gun; ) ||
          ( 
